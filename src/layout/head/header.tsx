84279f0a64ef6086932ecfe1bdc93ec1f8549975
wewealka228 <wewepoweralka228@mai.ru>
2024-07-26 14:14:52 +0300

    desing heder

--- src/layout/head/header.tsx
+++ src/layout/head/header.tsx
@@ -1,11 +1,11 @@
 import  React from 'react';
 import styled from 'styled-components';
 import { Logo } from '../../components/Logo';
-import { Menu } from '../../components/Menu';
+import { HeaderMenu } from './HeaderMenu';
 import {Container} from '../../components/Container';
 import { FlexWrapper } from '../../components/FlexWrapper';
 
-const items = ["Home","Skills","Works","About me","Contacts"]
+const items = ["Home","Skills","Works","Testimony","Contacts"]
 
 export const Header = () => {
     return (
@@ -13,7 +13,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
-                    <Menu menuItems={items}/>
+                    <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -22,6 +22,11 @@ export const Header = () => {
 
 
 const StyledHeader = styled.header`
-background-color: gray;
-height:58px;
+background-color:rgba(31, 31, 32, 0.9);
+padding:20px 0;
+position:fixed;
+top:0;
+right:0;
+left:0;
+z-index:99999;
 `
