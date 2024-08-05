a984d0ce7f3df4a8b1e824403296e513012d0e15
wewealka228 <wewepoweralka228@mai.ru>
2024-08-05 21:43:39 +0300

    adaption for trstimony section

--- src/layout/section/testimony/Testimony.tsx
+++ src/layout/section/testimony/Testimony.tsx
@@ -5,17 +5,20 @@ import { Icon } from "../../../components/Icon/Icon";
 import { Slider } from "../../../components/slider/Slider";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { IconWrapper } from "../skilladding/Skill";
+import { Container } from "../../../components/Container";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction={"column"} align={"center"}>
-                <IconWrapper>
-                    <Icon iconId={"doubleqoutes"}/>
-                </IconWrapper>
-            <Slider/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <IconWrapper>
+                        <Icon iconId={"doubleqoutes"} />
+                    </IconWrapper>
+                    <Slider />
+                </FlexWrapper>
+            </Container>
         </StyledTestimony>
     );
 };
