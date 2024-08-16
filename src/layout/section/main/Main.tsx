d07416635355738f16a1814655c608a014d0525b
wewealka228 <wewepoweralka228@mai.ru>
2024-08-16 16:33:49 +0300

    animation for photo main

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -3,6 +3,7 @@ import photo from '../../../assets/images/photo.jpg'
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Main: React.FC = () => {
     return (
@@ -12,7 +13,6 @@ export const Main: React.FC = () => {
                     <div>
                         <S.SmallText>Glad you could make it</S.SmallText>
                         <S.Name>I am <span>Vladimir Razumovich</span></S.Name>
-                        {/* <S.MainTitle>A Web Developer.</S.MainTitle> */}
                         <S.MainTitle>
                             <p>A Web Developer.</p>
                             <Typewriter
@@ -24,9 +24,11 @@ export const Main: React.FC = () => {
                             />
                         </S.MainTitle>
                     </div>
-                    <S.PhotoWrapper>
-                        <S.Photo src={photo} alt="" />
-                    </S.PhotoWrapper>
+                    <Tilt>
+                        <S.PhotoWrapper>
+                            <S.Photo src={photo} alt="" />
+                        </S.PhotoWrapper>
+                    </Tilt>
                 </FlexWrapper>
             </Container>
         </S.Main>
