1d17279053a865486e2138a11878f1fb1dc289be
wewealka228 <wewepoweralka228@mai.ru>
2024-08-21 20:10:04 +0300

    animationfor work section

--- src/layout/section/works/Works.tsx
+++ src/layout/section/works/Works.tsx
@@ -8,29 +8,29 @@ import { Container } from "../../../components/Container";
 import { S } from "./worksStyles/WorksStyles";
 import { useState } from "react";
 import { TabStatusType } from "./work/TabMenu";
+import { AnimatePresence, motion } from "framer-motion"
 
 
 
-
-const tabsItems:Array<{status:TabStatusType,title:string}> = [
+const tabsItems: Array<{ status: TabStatusType, title: string }> = [
     {
-        title:"All",
-        status:"all"
+        title: "All",
+        status: "all"
     },
 
     {
-        title:"LANDING PAGE",
-        status:"landing"
+        title: "LANDING PAGE",
+        status: "landing"
     },
 
     {
-        title:"REACT",
-        status:"react"
+        title: "REACT",
+        status: "react"
     },
 
     {
         title: "SPA",
-        status:"spa"
+        status: "spa"
     },
 ]
 
@@ -38,49 +38,101 @@ const worksData = [
     {
         title: "SocialNetwork",
         src: socialImg,
-        text:"In the near future, there will be my works there.",
-        type: "spa"
+        text: "In the near future, there will be my works there.",
+        type: "spa",
+        id: 1
     },
 
     {
         title: "Timer",
         src: timer,
-        text:"In the near future, there will be my works there.",
-        type: "react"
-    }
+        text: "In the near future, there will be my works there.",
+        type: "react",
+        id: 2
+    },
+
+    {
+        title: "SocialNetwork",
+        src: socialImg,
+        text: "In the near future, there will be my works there.",
+        type: "spa",
+        id: 3
+    },
+
+    {
+        title: "Timer",
+        src: timer,
+        text: "In the near future, there will be my works there.",
+        type: "react",
+        id: 4
+    },
+
+    {
+        title: "SocialNetwork",
+        src: socialImg,
+        text: "In the near future, there will be my works there.",
+        type: "spa",
+        id: 5
+    },
+
+    {
+        title: "Timer",
+        src: timer,
+        text: "In the near future, there will be my works there.",
+        type: "react",
+        id: 6
+    },
+
+
 ]
 
-export const Works:React.FC = () => {
+export const Works: React.FC = () => {
 
-    const [currentFilterStatus, setCurrentFilterStatus] = useState ("all");
+    const [currentFilterStatus, setCurrentFilterStatus] = useState("all");
     let filteredWorks = worksData;
 
     if (currentFilterStatus === "landing") {
-        filteredWorks= worksData.filter (work => work.type === "landing")
+        filteredWorks = worksData.filter(work => work.type === "landing")
     };
     if (currentFilterStatus === "react") {
-        filteredWorks= worksData.filter (work => work.type === "react")
+        filteredWorks = worksData.filter(work => work.type === "react")
     };
     if (currentFilterStatus === "spa") {
-        filteredWorks= worksData.filter (work => work.type === "spa")
+        filteredWorks = worksData.filter(work => work.type === "spa")
     };
 
-    function changeFilterStatus  (value:TabStatusType) {
-        setCurrentFilterStatus (value)
+    function changeFilterStatus(value: TabStatusType) {
+        setCurrentFilterStatus(value)
     }
 
     return (
         <S.Works id={"works"}>
             <Container>
                 <SectionTitle>My Works</SectionTitle>
-                <TabMenu tabsItems={tabsItems} changeFilterStatus={changeFilterStatus} currentFilterStatus={currentFilterStatus}/>
+                <TabMenu tabsItems={tabsItems} changeFilterStatus={changeFilterStatus} currentFilterStatus={currentFilterStatus} />
                 <FlexWrapper justify={"space-between"} align={"flex-start"} wrap={"wrap"}>
 
-                    {filteredWorks.map((w)=> {
-                        return <Work title={w.title}
-                                src={w.src}
-                                text={w.text} />
-                    })}
+                    <AnimatePresence>
+                        {filteredWorks.map((w) => {
+                            return (
+                                <motion.div style={{width:"400px", flexGrow: 1, maxWidth:"540px"}}
+                                    layout
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{opacity: 0}}
+                                    key={w.id}
+                                >
+                                    <Work 
+                                        title={w.title}
+                                        src={w.src}
+                                        text={w.text} 
+                                        key={w.id}
+                                    />
+                                </motion.div>
+                            )
+                        })}
+                    </AnimatePresence>
+
                 </FlexWrapper>
             </Container>
         </S.Works>
--- src/layout/section/works/worksStyles/WorksStyles.ts
+++ src/layout/section/works/worksStyles/WorksStyles.ts
@@ -14,8 +14,8 @@ position:relative;
 const Work = styled.div`
     background-color:${theme.colors.secondaryBg};
     
-    width:330px;
-    flex-grow: 1;
+    /* width:330px;
+    flex-grow: 1; */
 
     ${Link} {
         padding: 10px 0;
@@ -26,7 +26,7 @@ const Work = styled.div`
     }
 
     @media ${theme.media.desktop} {
-        max-width:540px;
+        /* max-width:540px; */
     }
 `
 
